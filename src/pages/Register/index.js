574d65b41170d6a51f06ab603612fbc9b17638b7
Gustavo Duarte <gduartemenezes@gmail.com>
2020-06-09 18:51:30 -0300

    Add Register page content and styles

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 
-import { Page, Container, Form, Input } from './styles';
+import { Link } from 'react-router-dom';
+import { Page, Container, Form, Input, SubmitButton, Info } from './styles';
+
+import logo from '../../assets/logo.png';
 
 export default class Register extends Component {
   render() {
     return (
       <Page>
         <Container>
+          <Info>
+            <img src={logo} alt="B Beers Logo" />
+
+            <p>
+              Make your account
+              <br /> Discover a new beer right next to you
+            </p>
+            <Link to="/">Back to home page.</Link>
+          </Info>
+
           <Form>
-            <Input />
-            <Input />
-            <Input />
+            <Input type="text" placeholder="Name" />
+            <Input type="text" placeholder="Email" />
+            <Input type="password" placeholder="Password" />
+            <SubmitButton> Register </SubmitButton>
           </Form>
         </Container>
       </Page>
--- src/pages/Register/styles.js
+++ src/pages/Register/styles.js
@@ -2,15 +2,84 @@ import styled from 'styled-components';
 
 import hop from '../../assets/hop.png';
 
-export const Page = styled.section`
+export const Page = styled.div`
   background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
     url(${hop});
   background-size: cover;
-  /* background-position: center; */
   height: 100vh;
 `;
 
-export const Container = styled.div``;
-export const Form = styled.form``;
-export const Input = styled.input``;
-export const SubmitButton = styled.button``;
+export const Container = styled.div`
+  width: 900px;
+  background: #eee;
+  height: 500px;
+  padding: 0;
+  border-radius: 8px;
+  position: absolute;
+  top: 15%;
+  left: 50%;
+  transform: translateX(-50%);
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Info = styled.div`
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  img {
+    width: 200px;
+    height: 200px;
+  }
+
+  p {
+    margin: 30px 0;
+    line-height: 170%;
+    text-align: center;
+  }
+
+  a {
+    text-decoration: none;
+    color: #555;
+    font-size: 80%;
+  }
+  a:hover {
+    opacity: 0.6;
+  }
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 70px auto;
+`;
+export const Input = styled.input`
+  width: 350px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+
+  & + input {
+    margin-top: 15px;
+  }
+`;
+export const SubmitButton = styled.button`
+  margin-top: 30px;
+  width: 300px;
+  padding: 10px;
+  border-radius: 8px;
+  border: 1px solid #c3410e;
+  background: #c3410e;
+  color: #fff;
+  transition: background 0.5s color 0.5s;
+
+  &:hover {
+    background: #94310a;
+    color: #ccc;
+  }
+`;
