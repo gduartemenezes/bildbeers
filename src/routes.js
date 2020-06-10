6f9331e2c9cd6a40394d6730e4f831ee65a730e7
Gustavo Duarte <gduartemenezes@gmail.com>
2020-06-10 15:55:23 -0300

    Add popular to routes

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,16 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import Beer from './pages/Beer';
 import Register from './pages/Register';
+import Popular from './pages/Popular';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Main} />
-        <Route path="/beer" exact component={Beer} />
-        <Route path="/register" exact component={Register} />
+        <Route path="/beer"  component={Beer} />
+        <Route path="/register" component={Register} />
+        <Route path="/popular"  component={Popular} />
       </Switch>
     </BrowserRouter>
   );
