73e1fdb3cd78c5031194d361232d45c2beb76a03
Gustavo Duarte <gduartemenezes@gmail.com>
2020-06-11 10:39:25 -0300

    Add Login page to routes

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import Main from './pages/Main';
 import Beer from './pages/Beer';
 import Register from './pages/Register';
 import Popular from './pages/Popular';
+import Login from './pages/Login';
 
 export default function Routes() {
   return (
@@ -12,6 +13,7 @@ export default function Routes() {
       <Route path="/" exact component={Main} />
       <Route path="/beer" component={Beer} />
       <Route path="/register" component={Register} />
+      <Route path="/login" component={Login} />
       <Route path="/popular" component={Popular} />
     </Switch>
   );
