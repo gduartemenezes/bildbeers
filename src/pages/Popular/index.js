9cf78abb974dcc8891328f21fbbab575dc08661e
Gustavo Duarte <gduartemenezes@gmail.com>
2020-06-13 10:45:11 -0300

    Add content and style to popular page

--- src/pages/Popular/index.js
+++ src/pages/Popular/index.js
@@ -1,13 +1,98 @@
-import React from 'react'
+import React from 'react';
 
-import { Page, Container } from './styles';
+import { Page, Container, BeerList, Info } from './styles';
 
 export default function Popular() {
   return (
     <Page>
       <Container>
+        <BeerList>
+          <li key={192}>
+            <img src="https://images.punkapi.com/v2/192.png" alt="beer" />
+            <Info>
+              <span name="name">Name:</span>
+              <p>Punk IPA</p>
+              <span name="brewer">Brewer:</span>
+              <p>Mr. Brewer Master</p>
+              <span name="tagline">Tags:</span>
+              <p>Post Modern Classic. Spiky. Tropical. Hoppy.</p>
 
+              <span name="abv">ABV:</span>
+              <p>6.0</p>
+            </Info>
+          </li>
+          <li key={192}>
+            <img src="https://images.punkapi.com/v2/192.png" alt="beer" />
+            <Info>
+              <span name="name">Name:</span>
+              <p>Punk IPA</p>
+              <span name="brewer">Brewer:</span>
+              <p>Mr. Brewer Master</p>
+              <span name="tagline">Tags:</span>
+              <p>Post Modern Classic. Spiky. Tropical. Hoppy.</p>
+
+              <span name="abv">ABV:</span>
+              <p>6.0</p>
+            </Info>
+          </li>
+          <li key={192}>
+            <img src="https://images.punkapi.com/v2/192.png" alt="beer" />
+            <Info>
+              <span name="name">Name:</span>
+              <p>Punk IPA</p>
+              <span name="brewer">Brewer:</span>
+              <p>Mr. Brewer Master</p>
+              <span name="tagline">Tags:</span>
+              <p>Post Modern Classic. Spiky. Tropical. Hoppy.</p>
+
+              <span name="abv">ABV:</span>
+              <p>6.0</p>
+            </Info>
+          </li>
+          <li key={192}>
+            <img src="https://images.punkapi.com/v2/192.png" alt="beer" />
+            <Info>
+              <span name="name">Name:</span>
+              <p>Punk IPA</p>
+              <span name="brewer">Brewer:</span>
+              <p>Mr. Brewer Master</p>
+              <span name="tagline">Tags:</span>
+              <p>Post Modern Classic. Spiky. Tropical. Hoppy.</p>
+
+              <span name="abv">ABV:</span>
+              <p>6.0</p>
+            </Info>
+          </li>
+          <li key={192}>
+            <img src="https://images.punkapi.com/v2/192.png" alt="beer" />
+            <Info>
+              <span name="name">Name:</span>
+              <p>Punk IPA</p>
+              <span name="brewer">Brewer:</span>
+              <p>Mr. Brewer Master</p>
+              <span name="tagline">Tags:</span>
+              <p>Post Modern Classic. Spiky. Tropical. Hoppy.</p>
+
+              <span name="abv">ABV:</span>
+              <p>6.0</p>
+            </Info>
+          </li>
+          <li key={192}>
+            <img src="https://images.punkapi.com/v2/192.png" alt="beer" />
+            <Info>
+              <span name="name">Name:</span>
+              <p>Punk IPA</p>
+              <span name="brewer">Brewer:</span>
+              <p>Mr. Brewer Master</p>
+              <span name="tagline">Tags:</span>
+              <p>Post Modern Classic. Spiky. Tropical. Hoppy.</p>
+
+              <span name="abv">ABV:</span>
+              <p>6.0</p>
+            </Info>
+          </li>
+        </BeerList>
       </Container>
     </Page>
-  )
+  );
 }
--- src/pages/Popular/styles.js
+++ src/pages/Popular/styles.js
@@ -11,7 +11,7 @@ export const Page = styled.div`
 
 export const Container = styled.div`
   width: 1100px;
-  background: #eee;
+  background: rgba(200, 200, 200, 0.4);
   border-radius: 8px;
   position: absolute;
   top: 15%;
@@ -21,4 +21,57 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+`;
+
+export const BeerList = styled.ul`
+  margin: 20px auto;
+
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 40px;
+  li {
+    cursor: pointer;
+
+    background: rgba(72, 60, 90, 0.4);
+    border-radius: 8px;
+    padding: 10px;
+    max-width: 300px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    text-align: center;
+    transition: transform 0.7s, background 0.4s;
+    img {
+      height: 200px;
+      margin: 15px;
+    }
+
+    &:hover {
+      transform: scale(1.15);
+      filter: brightness(80%);
+      background: #6c608a;
+    }
+  }
+`;
+
+export const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: #352f44;
+  color: #fff;
+  border-radius: 4px;
+  span {
+    font-size: 70%;
+    font-weight: bold;
+    margin: 10px 5px 0 0;
+  }
+  p {
+    margin: 0 10px;
+    font-size: 80%;
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+    line-height: 140%;
+  }
 `;
