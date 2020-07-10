26f3d5cb8ceb70e79507fef846f1867d2b6b9a39
Gustavo Duarte <gduartemenezes@gmail.com>
2020-07-10 11:39:41 -0300

    Add new pages to routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,15 +4,21 @@ import Route from './Route';
 
 import Main from '../pages/Main';
 import Beer from '../pages/Beer';
+import User from '../pages/User';
+
 import Register from '../pages/Register';
 import Popular from '../pages/Popular';
 import Login from '../pages/Login';
+import Search from '../pages/Search';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Main} />
-      <Route path="/beer" component={Beer} isPrivate />
+      <Route path="/beer" component={Beer} />
+      <Route path="/user" component={User} />
+      <Route path="/search" component={Search} />
+
       <Route path="/register" component={Register} />
       <Route path="/login" component={Login} />
       <Route path="/popular" component={Popular} isPrivate />
